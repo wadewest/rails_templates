9448bad44d52e9f9a2653c63bffb1f179ec55e95
Wade West <wwest81@gmail.com>
2011-02-25 10:08:25 -0600

    Failed to close a string.

--- haml_compass.rb
+++ haml_compass.rb
@@ -16,7 +16,7 @@ initializer "stylesheets.rb", <<-CODE
 require 'fileutils'
 FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
 Sass::Plugin.on_updating_stylesheet do |template, css|
-	puts "Compiling \#{template} to \#{css}
+	puts "Compiling \#{template} to \#{css}"
 end
 
 Rails.configuration.middleware.insert_before("Rack::Sendfile", "Rack::Static",
